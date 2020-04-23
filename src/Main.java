313ecec11f959dcdf6a8fe1f580eb45e945a1039
Sijan Shrestha <sijan@unlimit.com>
2020-04-23 07:42:21 +0545

    first commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String args[]) {
+        // data type, loop, control statement, operator (basic kura haru vayo)
+
+        // OOP Principal
+        /**
+         * 1. Class and Object
+         * 2. Encapsulation
+         * 3. Abstraction
+         * 4. Inheritance
+         * 5. Polymorphism
+         */
+
+        // Today's topic:
+        // Git and Github
+        // Class and Object, Constructor
+        // Method and its return types with parameters
+        // Scanner class
+        // Encapsulation and Abstraction
+        // Problem
+
+        /*
+            1. git init (initializes repository)
+            2. git add <file name> (add file to index)
+            3. git status (check status of working tree)
+            4. git commit (commit changes in index)
+            5. git push (push your code to central repo)
+            6. git pull (pull latest project from central/remote repo)
+         */
+
+
+
+    }
+}
